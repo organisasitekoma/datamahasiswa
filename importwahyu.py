217da73a327bcd2a7b05a39ca2a539ec29bbc37e
wahyupriawansyah <49294309+wahyupriawansyah@users.noreply.github.com>
2019-10-17 15:19:07 +0800

    Update importwahyu.py

--- importwahyu.py
+++ importwahyu.py
@@ -24,7 +24,7 @@ def main():
 	
 	waktu = tugasgeometri.menghitungwaktutempuh(s,v )
 	
-	print("\nWaktu yang di tempuh adalah",waktu, "menit" )
+	print("\nWaktu yang di tempuh adalah",waktu, "jam" )
 	
 	
 if __name__ =="__main__":
